ef5255e70c1a5b69d6e936c4eacafd4e579406c4
Haris Qazi <github@harisqazi.com>
2021-09-25 01:40:56 -0500

    update

--- leak_script.js
+++ leak_script.js
@@ -255,12 +255,49 @@ button.addEventListener("click", () => {
 });
 
 //Mouse Tracking //need to test this // https://www.tutorialspoint.com/javascript-focus
-let sampleEle = document.querySelector(".sample");
-document.body.addEventListener("mousemove", (event) => {
-   sampleEle.innerHTML = "X axis: " + event.x + " Y axis: " + event.y;
-});
+(function() {
+   var mousePos;
 
-document.getElementById("X").innerHTML = sampleEle;
+   document.onmousemove = handleMouseMove;
+   setInterval(getMousePosition, 100); // setInterval repeats every X ms
+   document.getElementById("X").innerHTML = mousePos;
+   function handleMouseMove(event) {
+       var dot, eventDoc, doc, body, pageX, pageY;
+
+       event = event || window.event; // IE-ism
+
+       // If pageX/Y aren't available and clientX/Y are,
+       // calculate pageX/Y - logic taken from jQuery.
+       // (This is to support old IE)
+       if (event.pageX == null && event.clientX != null) {
+           eventDoc = (event.target && event.target.ownerDocument) || document;
+           doc = eventDoc.documentElement;
+           body = eventDoc.body;
+
+           event.pageX = event.clientX +
+             (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+             (doc && doc.clientLeft || body && body.clientLeft || 0);
+           event.pageY = event.clientY +
+             (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+             (doc && doc.clientTop  || body && body.clientTop  || 0 );
+       }
+
+       mousePos = {
+           x: event.pageX,
+           y: event.pageY
+       };
+   }
+   function getMousePosition() {
+       var pos = mousePos;
+       if (!pos) {
+           // We haven't seen any movement yet
+       }
+       else {
+           // Use pos.x and pos.y
+       }
+   }
+   
+})();
 
 //IP Address
 window.onload = function () {
