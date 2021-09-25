3c055df689d3bb93e15c2d6fbd8407e923b8174e
Haris Qazi <github@harisqazi.com>
2021-09-25 01:47:50 -0500

    Update leak_script.js

--- leak_script.js
+++ leak_script.js
@@ -255,48 +255,17 @@ button.addEventListener("click", () => {
 });
 
 //Mouse Tracking //need to test this // https://www.tutorialspoint.com/javascript-focus
-(function() {
-   var mousePos;
-
-   document.onmousemove = handleMouseMove;
-   setInterval(getMousePosition, 100); // setInterval repeats every X ms
-   function handleMouseMove(event) {
-       var dot, eventDoc, doc, body, pageX, pageY;
-
-       event = event || window.event; // IE-ism
-
-       // If pageX/Y aren't available and clientX/Y are,
-       // calculate pageX/Y - logic taken from jQuery.
-       // (This is to support old IE)
-       if (event.pageX == null && event.clientX != null) {
-           eventDoc = (event.target && event.target.ownerDocument) || document;
-           doc = eventDoc.documentElement;
-           body = eventDoc.body;
-
-           event.pageX = event.clientX +
-             (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-             (doc && doc.clientLeft || body && body.clientLeft || 0);
-           event.pageY = event.clientY +
-             (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-             (doc && doc.clientTop  || body && body.clientTop  || 0 );
-       }
-
-       mousePos = {
-           x: event.pageX,
-           y: event.pageY
-       };
-   }
-   function getMousePosition() {
-       var pos = mousePos;
-       if (!pos) {
-           // We haven't seen any movement yet
-       }
-       else {
-           // Use pos.x and pos.y
-       }
-   }
-   
-})();
+var pointerX = -1;
+var pointerY = -1;
+document.onmousemove = function(event) {
+	pointerX = event.pageX;
+	pointerY = event.pageY;
+}
+setInterval(pointerCheck, 1000);
+function pointerCheck() {
+	//console.log('Cursor at: '+pointerX+', '+pointerY);
+   document.getElementById("X").innerHTML = 'Cursor at: '+pointerX+', '+pointerY;
+}
 document.getElementById("X").innerHTML = mousePos;
 //IP Address
 window.onload = function () {
