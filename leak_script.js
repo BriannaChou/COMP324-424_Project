9189317aed2932d05454dd045ad47f6373383839
Haris Qazi <github@harisqazi.com>
2021-10-01 00:27:40 -0500

    update

--- leak_script.js
+++ leak_script.js
@@ -340,3 +340,21 @@ var type = navigator.connection.type; //test this
 var rtt = navigator.connection.rtt;
 
 document.getElementById("network").innerHTML = effective_type + "<br> " + type + "<br>" +  "Round Trip Time = " + rtt;
+
+//Device type //https://abdessalam.dev/blog/detect-device-type-javascript/
+const getDeviceType = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+      return "tablet";
+    }
+    if (
+      /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+        ua
+      )
+    ) {
+      return "mobile";
+    }
+    return "desktop";
+  };
+  
+document.getElementById("device").innerHTML = getDeviceType();
