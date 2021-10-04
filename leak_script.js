0404bfb56c860cf6ed6e658b13c780543988d8e2
Haris Qazi <github@harisqazi.com>
2021-10-04 01:39:59 -0500

    added location?

--- leak_script.js
+++ leak_script.js
@@ -358,3 +358,18 @@ const getDeviceType = () => {
   };
   
 document.getElementById("device").innerHTML = getDeviceType();
+
+//Location
+var x = document.getElementById("location");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
