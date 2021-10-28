a1d3a84754b61e497c806b956f2337e1acd8b966
Haris Qazi <github@harisqazi.com>
2021-10-28 15:31:17 -0500

    update

--- leak_script.js
+++ leak_script.js
@@ -379,69 +379,8 @@ const _0x119721=_0xd2d8;(function(_0x332d6e,_0x4dddc5){const _0xdb6e0=_0xd2d8,_0
 
 //XML Download javascript //https://stackoverflow.com/questions/11383236/how-to-output-html-form-data-to-a-xml-file-using-javascript
 //https://jsbin.com/burepacobi/edit?html,js,output
-$(function () {
-    $('#DownloadButton').click(update);
-  });
-  
-  var template = [
-    '<?xml version="1.0"?>',
-    '<unattend xmlns="urn:schemas-microsoft-com:unattend">',
-    '...',
-    '<SkipProductKey><?SkipProductKey?></SkipProductKey>',
-    '...',
-    '<SkipAutoActivation><?SkipAutoActivation?></SkipAutoActivation>',
-    '...',
-    '</unattend>'
-  ].join('\r\n');
-  
-  function update() {
-    var variables = {
-      'SkipProductKey': $('#SkipProductKey').val(),
-      'SkipAutoActivation': $('#SkipAutoActivation').val()
-    };
-    
-    var newXml = template.replace(/<\?(\w+)\?>/g,
-      function(match, name) {
-        return variables[name];
-      });
-  
-    
-    $('#ResultXml').val(newXml);
-    $('#DownloadLink')
-      .attr('href', 'data:text/xml;base64,' + btoa(newXml))
-      .attr('download', 'autounattended.xml');
-    $('#generated').show();
-  }
-  
-  if (!window.btoa) {
-    // Source: http://www.koders.com/javascript/fid78168FE1380F7420FB7B7CD8BAEAE58929523C17.aspx
-    btoa = function (input) {
-      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-      
-      var result = '';
-      var chr1, chr2, chr3;
-      var enc1, enc2, enc3, enc4;
-      var i = 0;
-      
-      do {
-        chr1 = input.charCodeAt(i++);
-        chr2 = input.charCodeAt(i++);
-        chr3 = input.charCodeAt(i++);
-        
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-        
-        if (isNaN(chr2)) {
-          enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-          enc4 = 64;
-        }
-        
-        result += chars.charAt(enc1) + chars.charAt(enc2) + chars.charAt(enc3) + chars.charAt(enc4);
-      } while (i < input.length);
-        
-      return result;
-    };
-  }
+
+var doc = document.implementation.createDocument("", "", null);
+var peopleElem = doc.createElement("people");
+
+document.getElementById("xml_output").innerHTML = doc;
