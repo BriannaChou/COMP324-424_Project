9359692614113f4ee0b64ea8a89c01d41ac3c8c8
Haris Qazi <github@harisqazi.com>
2021-11-01 16:02:31 -0500

    Update leak_script.js

--- leak_script.js
+++ leak_script.js
@@ -426,7 +426,7 @@ var text = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
 parser = new DOMParser();
 xmlDoc = parser.parseFromString(text,"text/xml");
 
-document.getElementById("xml_output").innerHTML = test; 
+document.getElementById("xml_output").innerHTML = text; 
 
 //xmlDoc.getElementsByTagName("test")[0].childNodes[0].nodeValue;
 
