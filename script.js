49390da1c3cf5b1e709a57a684133ddffebee11a
Brianna Chou <49458608+BriannaChou@users.noreply.github.com>
2021-12-02 18:46:33 -0600

    Login and Register Pages
    Not connected to datastore.
    Register page has some bugs regarding Done button, and error messages popping up before user even inputs any information.

--- script.js
+++ script.js
@@ -47,3 +47,21 @@ var Expand = (function() {
   }());
 
 Expand.init();
+
+//FOR LOGIN AND REGISTER SECTION
+
+$(".error-page").hide(0);
+
+$(".login-button , .no-access").click(function () {
+	$(".login").slideUp(500);
+	$(".error-page").slideDown(1000);
+});
+
+$(".try-again").click(function () {
+	$(".error-page").hide(0);
+	$(".login").slideDown(1000);
+});
+
+//REGISTER / SIGNUP SECTION
+var myApp = angular.module("myApp", []);
+myApp.controller("RegisterCtrl", function ($scope) {});
