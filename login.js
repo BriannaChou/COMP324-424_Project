a8ebd3fc02a1df6af0a862aae16c493a552d6a1e
Haris Qazi <github@harisqazi.com>
2021-12-04 19:46:13 -0600

    password

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-var usernames = [
+/*var usernames = [
 	'userjalan',
 	'userbrianna',
 	'userharris'
@@ -17,5 +17,30 @@ for (var i = 0; i < username.length; i++) {
 		break;
 	} else {
 		alert('Failed!')
+	}
+}
+*/
+
+function loadpage() {
+	var usernames = [
+		'userjalan',
+		'userbrianna',
+		'userharris'
+	];
+	var passwords = [
+		'password',
+		'password123',
+		'passw0rd'
+	];
+	var username = 'userjalan';
+	var password = 'password';
+	
+	for (var i = 0; i < username.length; i++) {
+		if (username == usernames[i] && password == passwords[i]) {
+			alert('Successful!');
+			break;
+		} else {
+			alert('Failed!')
+		}
 	}
 }
